73d78b1c321b2c0f32725210cb99ad6db45df4d0
Rocklee0 <mshakil3847@gmail.com>
2024-12-24 01:31:13 +0600

    done

--- contains_duplicate_217.py
+++ contains_duplicate_217.py
@@ -1,8 +1,27 @@
 def containsDuplicate(nums):
     for i in range(len(nums)):
-        initial_value=nums[i]
-        for i in nums:
-            if initial_value==i:
+        initial_value=nums[i] #3
+        print(i) #3
+        for j_index, j in enumerate(nums):
+            if initial_value==j and i != j_index:
+                print(initial_value)
+                print('dddd')
                 return True
+    print(f'i is :{initial_value}')
     return False
-print(containsDuplicate([1,2,3]))
+print(containsDuplicate([1,2,3,3]))
+
+
+def contain(nums):
+    distinct=set(nums)
+    if len(nums)==len(distinct):
+        return False
+    else:
+        return True
+
+print(contain([1,2,2,3]))
+
+
+
+
+
