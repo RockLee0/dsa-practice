5014b5f945d4d8698cecedb5e330ab3a20179c9b
Rocklee0 <mshakil3847@gmail.com>
2024-12-26 02:30:45 +0600

    final done

--- two sums.py
+++ two sums.py
@@ -1,9 +1,33 @@
+#wrong approach
 def twoSum(nums, target):
-    for i in range(int(target/2)):
-        print(range(int(target/2)))
-        if i in nums and target-i in nums:
-            return [nums.index(i),nums.index(target-i)]
-    return []
+    if target==0:
+        for j in nums:
+            if j==0:
+                return [nums.index(j), nums.index(j, nums.index(j) + 1)]
+            if -j in nums:
+                return [nums.index(j),nums.index(-j)]
+    else:
+        for i in nums:
+            if target - i in nums:
+                if i!=target-i:
+                    return [nums.index(i), nums.index(target - i)]
+                else: return [nums.index(i),nums.index(i,nums.index(i)+1)]
+        return []
 
-print(twoSum([3,2,4],6))
+
+print(twoSum([0,4,3,0],0))
+print(twoSum([-1,-2,-3,-4,-5],-8))
+print(twoSum([3,3],6))
+
+
+#right approach
+def twoSum_(n,t):
+    s={}
+    for index,i in enumerate(n):
+        if t-i in s:
+            return [s[t-i],index]
+        s[i]=index
+    return []
+print(twoSum_([0,4,3,0],0))
+
 
