380e30a75679582839fe2ff0c82c1bfa21861ea6
Rocklee0 <mshakil3847@gmail.com>
2024-12-30 23:54:21 +0600

    checking with set

--- contains_duplicate_219.py
+++ contains_duplicate_219.py
@@ -12,3 +12,18 @@ class Solution:
 
     def a(self):
         return 'fuck you'
+
+    class Solution:
+
+        def removeElement(self, nums: List[int], val: int) -> int:
+
+            index = 0
+
+            for i in range(len(nums)):
+
+                if nums[i] != val:
+                    nums[index] = nums[i]
+
+                    index += 1
+
+            return index
